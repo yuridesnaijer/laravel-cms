6874c85d54a6deef875093418c48d69f750c26f7
Yuri de Snaijer <yuridesnaijer@gmail.com>
2014-11-25 20:42:12 +0100

    added html theme

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'test',
+			'database'  => 'morgenmiddag-2015',
 			'username'  => 'root',
 			'password'  => '',
 			'charset'   => 'utf8',
